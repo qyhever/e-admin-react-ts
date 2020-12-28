290e110de21546f5556afa606050e725aadfe4b5
v_yohqin <v_yohqin@tencent.com>
2020-12-28 17:23:46 +0800

    :sparkles: update dashboard/line

--- src/views/dashboard/components/line.tsx
+++ src/views/dashboard/components/line.tsx
@@ -1,11 +1,23 @@
-import React, { useRef } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 // import useChartResize from '@/hooks/useChartResize'
 import useEcharts from '@/hooks/useEcharts'
-import option from './option'
+import { getOption } from './option'
 
 const LineChart = () => {
   const chartRef = useRef<HTMLDivElement | null>(null)
-  useEcharts(chartRef.current, option as echarts.EChartOption)
+  const [option, setOption] = useState<echarts.EChartOption>({})
+
+  useEcharts(chartRef.current, option)
+
+  // 模拟异步数据
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setOption(getOption())
+    }, 20)
+    return () => {
+      clearTimeout(timer)
+    }
+  }, [])
 
   return (
     <div className="chart" ref={chartRef}></div>
